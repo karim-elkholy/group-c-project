3e0580ba9f6ed8398aabc6239e6591fbfaddc8b8
keiron-lee <keiron.lee@student.uts.edu.au>
2025-04-22 23:58:41 +1000

    Update input.h

--- include/user/input.h
+++ include/user/input.h
@@ -1,3 +1,26 @@
+/*
+#ifndef USER_INPUT_H
+#define USER_INPUT_H
+
+
+#include "core.h"
+
+patient_t *ask_patient_details();
+
+doctor_t *ask_doctor_details();
+
+int is_valid_blood_type(char *blood_type);
+
+int is_valid_gender(char *gender);
+
+int is_valid_email(char *email);
+
+int is_valid_phone_number(char *phone_number);
+
+/* Entry point for the program. Will prompt choices from here */
+void use();
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
