04efca223c8d05411abab99f50b83337a7aa05b7
keiron-lee <keiron.lee@student.uts.edu.au>
2025-04-25 13:50:07 +1000

    Update user_interface.h

--- include/user/user_interface.h
+++ include/user/user_interface.h
@@ -1,5 +1,16 @@
+/*******************************************************************************
+ * File: user_interface.h
+ * Description: Declaration for the CLI entry point of the hospital app.
+ * Author: Keiron Lee
+ ******************************************************************************/
+
 #ifndef USER_INTERFACE_H
 #define USER_INTERFACE_H
+
+/*******************************************************************************
+ * Entry Point for the Text-Based User Interface
+ * Author: Keiron Lee
+ ******************************************************************************/
 
 void use();
 
